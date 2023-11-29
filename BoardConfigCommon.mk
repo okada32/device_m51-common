5a3a3030543129590c4a0d5306803d7b9717f385
Okada32 <ruinedmind2001@gmail.com>
2023-11-29 16:45:16 +0100

    Update Kernel Configs

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -131,6 +131,7 @@ DEVICE_MATRIX_FILE := \
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 androidboot.usbcontroller=a600000.dwc3 firmware_class.path=/vendor/firmware_mnt/image nokaslr printk.devkmsg=on
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_APPEND_DTB := true
 BOARD_KERNEL_OFFSET := 0x00008000
@@ -141,7 +142,7 @@ BOARD_RAMDISK_OFFSET := 0x02000000
 BOARD_MKBOOTIMG_ARGS := --kernel_offset $(BOARD_KERNEL_OFFSET) --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_SOURCE := kernel/samsung/sm6150
+TARGET_KERNEL_SOURCE := kernel/samsung/m51
 TARGET_KERNEL_VERSION := 4.14
 
 # Keymaster
