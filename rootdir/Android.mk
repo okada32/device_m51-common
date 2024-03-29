0c407e85c486da0d9b2aa36c54e611f3cda5ad36
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-03-29 13:01:31 +0100

    rootdir: Move ueventd.rc to Android.bp
    Change-Id: I82c9289780b4d57f3e3b2d93f04d42e068ff7f3f

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -40,12 +40,3 @@ else
 LOCAL_SRC_FILES    := vendor/etc/fstab.qcom
 endif
 include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := ueventd.qcom.rc
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
-LOCAL_MODULE_STEM  := ueventd.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_SRC_FILES    := vendor/ueventd.qcom.rc
-include $(BUILD_PREBUILT)
