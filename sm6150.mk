749fb9866b0916ccf2f9fbf1b08b8ea906c6d461
Yuichigojo <ruinedmind2001@gmail.com>
2024-03-30 21:18:52 +0530

    Bring Back android.hardware.usb@1.1.vendor

--- sm6150.mk
+++ sm6150.mk
@@ -597,6 +597,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0.vendor \
     android.hardware.usb@1.3-service.dual_role_usb \
+    android.hardware.usb@1.1.vendor \
 
 # Vendor Service Manager
 PRODUCT_PACKAGES += \
